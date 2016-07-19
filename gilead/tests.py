1042366a2dcf50d983326da96150807a33f7d725
Matthew Alexander LaChance <matt@istheguy.com>
2016-07-19 10:47:41 -0400

    Write two failing tests

--- gilead/tests.py
+++ gilead/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+HASH_HELP_SUBSTR = "I assumed your browser would support JavaScript"
+
+class TestHashUrlView (TestCase):
+
+    def test_get_hash_url_uses_js_redirect (self):
+        response = self.client.get("/i")
+
+        self.assertContains(response, HASH_HELP_SUBSTR)
+
+    def test_post_hash_url_creates_ajax_page (self):
+        response = self.client.post("/i",
+                data = {"url": "/some/full/url"})
+
+        self.assertNotContains(response, HASH_HELP_SUBSTR)
